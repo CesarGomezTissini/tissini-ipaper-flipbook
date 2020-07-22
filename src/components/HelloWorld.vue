30a460bfefa11944229ac69b521f263fd55dec2f
Juan Loaiza <juanl@tissini.com>
2020-07-22 10:23:01 -0500

    feat: bug iframe

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,28 +3,27 @@
     <v-row>
       <v-col cols="12">
         <v-row align="center" justify="center">
-          <div class="iframe-loading" v-if="iframeLoading">
+          <!-- <div class="iframe-loading" v-if="iframeLoading">
             iframe loading...
-          </div>
-          <div style="width:100%">
-            <iframe
-              style="width: 100%; height: 640px"
-              ref="frame"
-              :style="`display: ${iframeLoading ? 'none' : 'block'}`"
-              src="https://viewer.ipaper.io/tissini/catalogo-textil-primavera-verano-2020/#/"
-              @load="onLoad"
-              @iframe-load="onIframeLoad"
-              @onSpreadChange="onAnyAction"
-              @onPageElementClick="itemDetail"
-              @onBasketClick="onOpenBasket"
-              frameborder="0"
-              allow="fullscreen;autoplay"
-              allowfullscreen
-              webkitallowfullscreen
-              mozallowfullscreen
-            >
-            </iframe>
-          </div>
+          </div> -->
+          <!-- <div> -->
+          <iframe
+            style="width: 100%; height: 550px"
+            ref="frame"
+            src="https://viewer.ipaper.io/tissini/catalogo-textil-primavera-verano-2020/"
+            @load="onLoad"
+            @iframe-load="onIframeLoad"
+            @onSpreadChange="onAnyAction"
+            @onPageElementClick="itemDetail"
+            @onBasketClick="onOpenBasket"
+            frameborder="0"
+            allow="fullscreen;autoplay"
+            allowfullscreen
+            webkitallowfullscreen
+            mozallowfullscreen
+          >
+          </iframe>
+          <!-- </div> -->
         </v-row>
       </v-col>
     </v-row>
